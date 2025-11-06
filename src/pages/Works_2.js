f7bf6f6784d4509f76fc123b43cc0c8becf16ab6
mingzheng <mingchang969@gmail.com>
2025-11-06 16:09:57 +0800

    crossOrigin

--- src/pages/Works_2.js
+++ src/pages/Works_2.js
@@ -160,6 +160,7 @@ const Works_2 = forwardRef(({ id }, ref) => {
                           ref={videoRef}
                           key={activeTab.id}
                           src={activeTab.image}
+                          crossOrigin="anonymous"
                           preload="metadata"
                           autoPlay
                           loop
